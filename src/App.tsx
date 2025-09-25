e6a3df3f2865dab4e5740fb1220c95973f38ccfa
DEMO <kritybade9@gmail.com>
2025-09-25 14:21:55 +0545

    added package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
