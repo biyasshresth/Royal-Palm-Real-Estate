e9ca0393c0d81e84eeee494a1a037eab1154ff98
T-Mart <T.Mart@gmail.com>
2025-10-10 11:18:09 +0545

    "show password"

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.svg";
 import { useNavigate } from "react-router-dom";
 import { UserCredentials } from "types/User";
 import UserService from "../services/user/UserService";
-
+import { Eye, EyeOff } from "lucide-react";
 interface LoginProps {
   setIsLoggedIn: (loggedIn: boolean) => void;
 }
